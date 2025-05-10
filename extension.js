7b57b3a73a36d98f57d53c4a2ab7e1826345f823
diandian157 <X94106038@gmail.com>
2025-05-10 20:24:34 +0800

    一将成名布局调整，版本号变更

--- extension.js
+++ extension.js
@@ -11581,7 +11581,7 @@ export default async function () {
 			pack.intro = (pack => {
 				let log = [
 					`魔改十周年UI ${pack.version}`,
-					"最低适配：v1.10.18",
+					"最低适配：v1.10.17.3",
 					"bugfix",
 					"OL样式联机适配",
 					"新版本函数跟进",
