be73beecfb0f8f113f7477f269be90500d1c9336
mengxinzxz <2223529500@qq.com>
2025-10-19 14:02:17 +0800

    Revert "补一个game.check"
    This reverts commit 9f2fc414f9f54893c89374f44021023e8679c461.

--- js/meihua.js
+++ js/meihua.js
@@ -1274,7 +1274,6 @@ decadeModule.import(function (lib, game, ui, get, ai, _status) {
 			const idx = ui.selected.cards.indexOf(card.relatedCard);
 			if (idx !== -1) ui.selected.cards.splice(idx, 1);
 		}
-		game.check();
 	}
 	const cardsProxy = new Proxy(ui.selected.cards, {
 		get(target, prop) {
