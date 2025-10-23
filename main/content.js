719e94d3714029253bc61a7046dccc6f94c1e6ff
yuexiaolingmeng <741144652@qq.com>
2025-10-23 14:29:02 +0800

    手杀样式阵亡优化

--- main/content.js
+++ main/content.js
@@ -933,15 +933,21 @@ export async function content(config, pack) {
 									url = decadeUIPath + "image/decorationh/dead3_" + identity + ".png";
 								} else if (decadeUI.config.newDecadeStyle === "codename") {
 									url = decadeUIPath + "image/decoration_code/dead_" + identity + ".png";
+								} else if (goon) {
+									url = decadeUIPath + "image/decoration/dead_" + identity + ".png";
 								} else {
-									url = decadeUIPath + "image/decoration" + (goon ? "/dead" : "s/dead2") + "_" + identity + ".png";
+									if (this != game.me) {
+										url = decadeUIPath + "image/decorations/dead2_" + identity + ".png";
+									} else {
+										url = decadeUIPath + "image/decorations/dead2_me.png";
+									}
 								}
 								image.onerror = function () {
 									that.node.dieidentity.innerHTML = decadeUI.getPlayerIdentity(that, that.identity, true) + "<br>阵亡";
 								};
 								// 离开效果仅在移动版样式下启用
 								if ((that._trueMe || that) != game.me && that != game.me && lib.config.extension_十周年UI_newDecadeStyle === "off") {
-									that.node.dieidentity.innerHTML = '<div style="width:21px; height:81px; left:18px; top:-12px; position:absolute; background-image: url(' + lib.assetURL + 'extension/十周年UI/assets/image/likai.png);background-size: 100% 100%;"></div>';
+									that.node.dieidentity.innerHTML = '<div style="width:21px; height:81px; left:22.5px; top:-12px; position:absolute; background-image: url(' + lib.assetURL + 'extension/十周年UI/assets/image/likai.png);background-size: 100% 100%;"></div>';
 								} else {
 									that.node.dieidentity.innerHTML = "";
 								}
